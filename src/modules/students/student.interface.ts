d68c5dd54b2973ca403d1ec2078db84d43deba77
sohel-tech4 <soheltech4@gmail.com>
2024-10-16 08:52:33 +0600

    started second project

---
+++ src/modules/students/student.interface.ts
@@ -0,0 +1,40 @@
+// import { Schema, model, connect } from 'mongoose';
+
+export type UserName = {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+};
+
+export type Gurdian = {
+  fatherName: string;
+  fatherOccupation: string;
+  fatherContactNo: string;
+  motherName: string;
+  motherOccupation: string;
+  motherContactNo: string;
+};
+
+export type LocalGurdian = {
+  name: string;
+  occupation: string;
+  contactNo: string;
+  address: string;
+};
+
+export type Student = {
+  id: string;
+  name: UserName;
+  gender: 'male' | 'female';
+  dateOfBirth: string;
+  email: string;
+  contactNo: string;
+  emergencyConcatctNo: string;
+  bloodGroup: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+  presentAddress: string;
+  permanentAddress: string;
+  gurdian: Gurdian;
+  localGurdian: LocalGurdian;
+  profileImg?: string;
+  isActive: 'active' | 'blocked';
+};
---
+++ src/modules/students/student.model.ts
@@ -0,0 +1,43 @@
+import { Schema, model } from 'mongoose';
+import { Gurdian, LocalGurdian, Student, UserName } from './student.interface';
+
+const UserName = new Schema<UserName>({
+  firstName: { type: String, required: true },
+  middleName: { type: String, required: true },
+  lastName: { type: String, required: true },
+});
+
+const GurdianSchema = new Schema<Gurdian>({
+  fatherName: { type: String, required: true },
+  fatherOccupation: { type: String, required: true },
+  fatherContactNo: { type: String, required: true },
+  motherName: { type: String, required: true },
+  motherOccupation: { type: String, required: true },
+  motherContactNo: { type: String, required: true },
+});
+
+const LocalGurdainSchema = new Schema<LocalGurdian>({
+  name: { type: String, required: true },
+  occupation: { type: String, required: true },
+  contactNo: { type: String, required: true },
+  address: { type: String, required: true },
+});
+
+const studentSchema = new Schema<Student>({
+  id: { type: String },
+  name: UserName,
+  gender: ['male', 'female'],
+  dateOfBirth: { type: String },
+  email: { type: String, required: true },
+  contactNo: { type: String, required: true },
+  emergencyConcatctNo: { type: String, required: true },
+  bloodGroup: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
+  presentAddress: { type: String, required: true },
+  permanentAddress: { type: String, required: true },
+  gurdian: GurdianSchema,
+  localGurdian: LocalGurdainSchema,
+  profileImg: { type: String },
+  isActive: ['active', 'blocked'],
+});
+
+export const student = model<Student>('student', studentSchema);
